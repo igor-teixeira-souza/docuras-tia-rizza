551dd248aecfb4a1d6548a8789c9cbd0b38502b5
igor-teixeira-souza <teixeiraigor525@gmai.com>
2026-03-27 16:51:53 -0300

    fix: corrigindo export do connectDB

--- backend/src/database/mongo.js
+++ backend/src/database/mongo.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`MongoDB conectado: ${conn.connection.host}`);
+    console.log("MongoDB conectado:", conn.connection.host);
   } catch (error) {
     console.error("Erro ao conectar no MongoDB:", error);
     process.exit(1);
