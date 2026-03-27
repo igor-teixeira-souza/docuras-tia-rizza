fa1b871d604c9670c150a7f3b42150d62a902230
igor-teixeira-souza <teixeiraigor525@gmai.com>
2026-03-27 16:47:19 -0300

    Mongo fixed

--- backend/src/database/mongo.js
+++ backend/src/database/mongo.js
@@ -1,17 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-try {
-const conn = await mongoose.connect(process.env.MONGO_URI);
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-```
-console.log("MongoDB conectado:", conn.connection.host);
-```
-
-} catch (error) {
-console.error("Erro ao conectar no MongoDB:", error);
-process.exit(1);
-}
+    console.log(`MongoDB conectado: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("Erro ao conectar no MongoDB:", error);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
