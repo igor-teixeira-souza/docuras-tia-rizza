dc1be9707a6d4d73a9dca979d058800a5470ccfa
igor-teixeira-souza <teixeiraigor525@gmai.com>
2026-03-30 15:55:59 -0300

    build funcionando

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000", // porta do backend
-        changeOrigin: true,
-      },
-    },
-  },
-});
+  plugins: [react()],
+})
